a44705ca224f38373141e430f7ddec99cebf0d74
Ian Davis <iandavis0527@gmail.com>
2022-02-15 23:13:42 -0500

    Removing index.html as j2 file is all we need and fixing package_data

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="psychojs_generator",
-    version="0.0.9",
+    version="0.0.10",
     author="Ian Davis",
     author_email="ian.davis.18.ctr@us.af.mil",
     description="A utility package that can generate nodejs and docker projects to host a custom psychojs project on the web, allowing CSV file uploads via NodeJS and multer.",
@@ -10,19 +10,11 @@ setup(
     license="BSD",
     packages=["psychojs_generator"],
     install_requires=["jinja2"],
-    package_data=[
-        (
-            "psychojs_generator",
-            [
-                "templates/example.js",
-                "templates/index.html.j2",
-                "templates/package.json",
-                "templates/server_config.json",
-                "templates/server.js",
-                "templates/wrapper.js",
-            ],
-        ),
-    ],
+    package_data={
+        "psychojs_generator": [
+            "templates/*.*",
+        ],
+    },
     entry_points={
         "console_scripts": [
             "create-psychojs-project=psychojs_generator.build_project:main",
