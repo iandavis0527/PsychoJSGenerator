647f4498ac3ff389216cbd84d9885ed8c8c2fa17
Ian Davis <iandavis0527@gmail.com>
2022-02-16 09:06:32 -0500

    bumping version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="psychojs_generator",
-    version="0.0.17",
+    version="0.0.18",
     author="Ian Davis",
     author_email="ian.davis.18.ctr@us.af.mil",
     description="A utility package that can generate nodejs and docker projects to host a custom psychojs project on the web, allowing CSV file uploads via NodeJS and multer.",
