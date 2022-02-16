82d00b4d3d18c9b888b1f68a41bd2ce13aaa2661
Ian Davis <iandavis0527@gmail.com>
2022-02-15 22:53:23 -0500

    Defining psychojs entry point script "create-psychojs-project"

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
+from importlib_metadata import entry_points
 from setuptools import setup, find_packages
 
 setup(
     name="psychojs_generator",
-    version="0.0.3",
+    version="0.0.4",
     author="Ian Davis",
     author_email="ian.davis.18.ctr@us.af.mil",
     description="A utility package that can generate nodejs and docker projects to host a custom psychojs project on the web, allowing CSV file uploads via NodeJS and multer.",
@@ -10,4 +11,9 @@ setup(
     license="BSD",
     packages=find_packages(),
     install_requires=["jinja2"],
+    entry_points={
+        "console_scripts": [
+            "create-psychojs-project=psychojs_generator.build_project",
+        ]
+    },
 )
