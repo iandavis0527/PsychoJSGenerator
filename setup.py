3cbbf011a5be350ca72552fd9aa4d878073156c7
Ian Davis <iandavis0527@gmail.com>
2022-02-15 22:49:43 -0500

    Ficking packages list

--- setup.py
+++ setup.py
@@ -2,14 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name="psychojs_generator",
-    version="0.0.2",
+    version="0.0.3",
     author="Ian Davis",
     author_email="ian.davis.18.ctr@us.af.mil",
-    description=(
-        "A utility package that can generate nodejs and docker projects to host a custom psychojs project on the web, allowing CSV file uploads via NodeJS and multer."
-    ),
+    description="A utility package that can generate nodejs and docker projects to host a custom psychojs project on the web, allowing CSV file uploads via NodeJS and multer.",
     include_package_data=True,
     license="BSD",
-    packages=[find_packages()],
+    packages=find_packages(),
     install_requires=["jinja2"],
 )
