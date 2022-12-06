91a6f8f6c12c93ec85d4a344a4daa6fdaa3565fe
Ian Davis <iandavis0527@gmail.com>
2022-12-06 14:25:53 -0500

    fixing the directory tree listing function and consequently the proper handling and hosting of asset imagery for web experiments

--- psychojs_generator/templates/server.js
+++ psychojs_generator/templates/server.js
@@ -10,17 +10,17 @@ const cookieParser = require("cookie-parser");
 
 let RESULT_DIRECTORY = path.join(__dirname, config.result_directory);
 let upload = multer({
-  storage: multer.diskStorage({
-    destination: RESULT_DIRECTORY,
-    filename: function (req, file, cb) {
-      cb(null, file.originalname);
-    },
-  }),
+    storage: multer.diskStorage({
+        destination: RESULT_DIRECTORY,
+        filename: function (req, file, cb) {
+            cb(null, file.originalname);
+        },
+    }),
 });
 
 function redirect(res, target, redirectUrl) {
-  res.cookie("redirectUrl", redirectUrl);
-  res.redirect(target);
+    res.cookie("redirectUrl", redirectUrl);
+    res.redirect(target);
 }
 
 app.set("view engine", "ejs");
@@ -30,88 +30,117 @@ app.use(cookieParser());
 app.use(express.urlencoded());
 
 app.get("/config.json", async (req, res, next) => {
-  res.set("Content-Type", "application/json");
-  res.send({
-    experiment: {
-      name: config.project_name,
-      fullpath: config.project_name,
-    },
+    res.set("Content-Type", "application/json");
+    res.send({
+        experiment: {
+            name: config.project_name,
+            fullpath: config.project_name,
+        },
 
-    pavlovia: {
-      URL: config.server_url,
-    },
+        pavlovia: {
+            URL: config.server_url,
+        },
 
-    gitlab: {
-      projectId: 000000,
-    },
-  });
+        gitlab: {
+            projectId: 000000,
+        },
+    });
 });
 
 app.post("/results", upload.single("result"), async (req, res, next) => {
-  res.end("uploaded");
+    res.end("uploaded");
 });
 
+function flattenedTree(listing, namePrefix = "") {
+    const children = [];
+
+    for (let i = 0; i < listing.length; i++) {
+        let entry = listing[i];
+        children.push({ name: namePrefix + entry.name, path: entry.path });
+
+        if (
+            entry.children !== undefined &&
+            entry.children !== null &&
+            entry.children.length > 0
+        ) {
+            children.push(
+                ...flattenedTree(
+                    entry.children,
+                    (namePrefix = "." + entry.path + "/")
+                )
+            );
+        }
+    }
+
+    return children;
+}
+
 app.get("/resources/list", (req, res, next) => {
-  const tree = dirTree(config.resource_directory).children.map((entry) => ({
-    name: entry.name,
-    path: entry.path.replace(
-      config.resource_directory.substring(2),
-      "/resources"
-    ),
-  }));
-  res.set("Content-Type", "application/json");
-  res.send(tree);
+    const tree = flattenedTree(dirTree(config.resource_directory).children).map(
+        (entry) => ({
+            name: entry.name.replace(
+                config.resource_directory.substring(2),
+                ""
+            ),
+            path: entry.path.replace(
+                config.resource_directory.substring(2),
+                "/resources"
+            ),
+        })
+    );
+    res.set("Content-Type", "application/json");
+    res.send(tree);
 });
 
 app.get("/resources/:file", (req, res, next) => {
-  const filepath = path.resolve(config.resource_directory, req.params.file);
-  res.sendFile(filepath);
+    const filepath = path.resolve(config.resource_directory, req.params.file);
+    res.sendFile(filepath);
 });
 
 app.get("/login", (req, res, next) => {
-  if (auth.isAuthenticated(req, res)) {
-    res.redirect(req.cookies.redirectUrl);
-  } else {
-    res.render("login");
-  }
+    if (auth.isAuthenticated(req, res)) {
+        res.redirect(req.cookies.redirectUrl);
+    } else {
+        res.render("login");
+    }
 });
 
 app.post("/login", async (req, res, next) => {
-  if (auth.isAuthenticated(req, res)) {
-    console.debug(
-      `user already authenticated, redirecting to ${req.cookies.redirectUrl}`
-    );
-    res.redirect(req.cookies.redirectUrl);
-  } else if (await auth.authenticate(req, res)) {
-    console.debug(
-      `user authenticated successfully, redirecting to ${req.cookies.redirectUrl}`
-    );
-    res.redirect(req.cookies.redirectUrl);
-  } else {
-    res.render("login", { errors: true });
-  }
+    if (auth.isAuthenticated(req, res)) {
+        console.debug(
+            `user already authenticated, redirecting to ${req.cookies.redirectUrl}`
+        );
+        res.redirect(req.cookies.redirectUrl);
+    } else if (await auth.authenticate(req, res)) {
+        console.debug(
+            `user authenticated successfully, redirecting to ${req.cookies.redirectUrl}`
+        );
+        res.redirect(req.cookies.redirectUrl);
+    } else {
+        res.render("login", { errors: true });
+    }
 });
 
 app.use("/export", (req, res, next) => {
-  const loggedIn = auth.isAuthenticated(req, res);
+    const loggedIn = auth.isAuthenticated(req, res);
 
-  if (!loggedIn) {
-    redirect(res, "/login", "/export");
-    return;
-  }
+    if (!loggedIn) {
+        redirect(res, "/login", "/export");
+        return;
+    }
 
-  console.debug("using express.static");
-  next();
+    console.debug("using express.static");
+    next();
 });
 
 app.use(
-  "/export",
-  express.static(RESULT_DIRECTORY),
-  serveIndex(RESULT_DIRECTORY)
+    "/export",
+    express.static(RESULT_DIRECTORY),
+    serveIndex(RESULT_DIRECTORY)
 );
 
 app.use(express.static("experiment"));
 
 app.listen(config.port, () => {
-  console.log(`Server listening on port ${config.port}`);
+    console.log(`Server listening on port ${config.port}`);
 });
