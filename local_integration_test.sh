23bb38c833a2b957158c710537f62c0c693d1caa
Ian Davis <iandavis0527@gmail.com>
2023-03-03 09:00:21 -0500

    udpating integration test and gitignore

--- local_integration_test.sh
+++ local_integration_test.sh
@@ -1,8 +1,11 @@
-rm -rf Mturk_IntExp_Gdn/
-python -m psychojs_generator.build_project integration-testing-project/html/Mturk_IntExp_Gdn.js --vm_name vm-online-experiments2 --vm_username iandavis --server_port 4000 --latest_version
+rm -rf v2Mturk_IntExp_Gdn/
+python -m psychojs_generator.build_project integration-testing-project/html/v2Mturk_IntExp_Gdn.js --vm_name vm-online-experiments2 --vm_username iandavis --server_port 4000 --latest_version
 
-rm -rf Mturk_IntExp_Gdn/id_rsa
-rm -rf Mturk_IntExp_Gdn/id_rsa.pub
+rm -rf v2Mturk_IntExp_Gdn/id_rsa
+rm -rf v2Mturk_IntExp_Gdn/id_rsa.pub
 
-cp ~/.ssh/id_rsa Mturk_IntExp_Gdn/
-cp ~/.ssh/id_rsa.pub Mturk_IntExp_Gdn/
+cp ~/.ssh/id_rsa v2Mturk_IntExp_Gdn/
+cp ~/.ssh/id_rsa.pub v2Mturk_IntExp_Gdn/
+
+cd v2Mturk_IntExp_Gdn/
+npm run start
